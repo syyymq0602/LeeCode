b5789370bd4b494786bd5927c94627ae0539b37b
tpl <tpl_vis_group@outlook.com>
2021-01-14 13:14:46 +0800

    TwoSum

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LeeCode
 Codes for algorithm
+
+### 两数之和（TwoSum）
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+例：
+    输入：nums = [2,7,11,15], target = 9
+    输出：[0,1]
---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {2, 7, 11, 15};
+            int target = 9;
+            int[] res = TwoSum(nums, target);
+            Console.WriteLine(res[0]+","+res[1]);
+        }
+        private static int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}

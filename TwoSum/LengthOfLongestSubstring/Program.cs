b7778ae398aa8e2e7371f2a22f9ffb236aad71f3
tpl <tpl_vis_group@outlook.com>
2021-03-06 20:45:47 +0800

    LeeCode 3

---
+++ TwoSum/LengthOfLongestSubstring/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LengthOfLongestSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "asdfasdfs";
+            var str = CheckRepeated(s);
+            Console.WriteLine(str);
+        }
+
+        private static int CheckRepeated(string s)
+        {
+            int maxNum = 0 , index = 0;
+            char[] chars = s.ToCharArray();
+            for (int i = 0; i < chars.Length; i++) {
+                int j = s.IndexOf(chars[i], index);
+                if(j!=i){
+                    index = j+1;
+                }
+                maxNum = Math.Max(maxNum,i-index+1);
+            }
+            return maxNum;
+        }
+    }
+}

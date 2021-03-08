011a454fec9259dd0cac02b9d0ca1eb4e07acc1f
tpl <tpl_vis_group@outlook.com>
2021-03-08 22:05:27 +0800

    LeeCode 6未完成

---
+++ TwoSum/Convert/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "PAYPALISHIRING";
+            Console.WriteLine(Convert(str,3));
+        }
+
+        private static string Convert(string s, int numRows)
+        {
+            char[][] chars = new char[numRows][];
+            char[] str = s.ToCharArray();
+            int j = -1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (j == 0 || j == 4)
+                    j = -j;
+            }
+            return "s";
+        }
+    }
+}

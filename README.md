01d52509c498d919af2c6e05b3753b4472e2a527
tpl <tpl_vis_group@outlook.com>
2021-03-11 14:42:35 +0800

    LeeCode 6 Success

--- README.md
+++ README.md
@@ -31,3 +31,17 @@ Codes for algorithm
 输入：s = "babad"
 输出："bab"
 解释："aba" 同样是符合题意的答案。
+
+### Z 字形变换（Convert）
+将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。<br>
+比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：<br>
+```
+P   A   H   N
+A P L S I I G
+Y   I   R
+```
+之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。<br>
+```
+输入：s = "PAYPALISHIRING", numRows = 3
+输出："PAHNAPLSIIGYIR"
+```
--- TwoSum/Convert/Program.cs
+++ TwoSum/Convert/Program.cs
@@ -7,14 +7,15 @@ namespace Convert
     {
         static void Main(string[] args)
         {
-            string str = "PAYPALISHIRING";
-            Console.WriteLine(Convert(str,3));
+            string str = "ABC";
+            Console.WriteLine(Convert(str,1));
         }
 
         private static string Convert(string s, int numRows)
         {
-            if (s.Length <= 1)
+            if (s.Length <= 1 || numRows == 1)
                 return s;
+            // 头尾常规字符间的距离
             int step = 2 * numRows - 2;
             int index = 0;
             int n = s.Length;
@@ -22,7 +23,9 @@ namespace Convert
             string ret = "";
             for(int i=0;i<numRows;i++)
             {
+                // 每行字符索引值
                 index = i;
+                // 中间字符之间的距离
                 add = 2 * i;
                 while(index < n)
                 {

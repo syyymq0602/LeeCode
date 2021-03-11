05ca1c4a98a0d766d07c29f4189b3cd65e62ba06
syyymq0602 <295217270@qq.com>
2021-03-11 22:48:33 +0800

    LeeCode 9

---
+++ TwoSum/isPalindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace isPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(2332));
+        }
+
+        private static bool IsPalindrome(int x)
+        {
+            var str = x.ToString();
+            char[] chars = str.ToCharArray();
+            for (int i = 0; i < chars.Length/2; i++)
+            {
+                var m = chars[i];
+                chars[i] = chars[str.Length - 1 - i];
+                chars[str.Length - 1 - i] = m;
+            }
+
+            string s = new string(chars);
+            if (s == str)
+                return true;
+            return false;
+        }
+    }
+}

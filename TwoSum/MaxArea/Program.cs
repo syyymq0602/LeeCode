dfe39c6b64904e3497c549ff48e9bd4aaf8bea4b
tpl <tpl_vis_group@outlook.com>
2021-03-15 19:43:23 +0800

    LeeCode 11 双指针

---
+++ TwoSum/MaxArea/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MaxArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = {4,6,4};
+            Console.WriteLine(MaxArea(num));
+        }
+        // 双指针
+        private static int MaxArea(int[] height)
+        {
+            int left = 0, right = height.Length - 1;
+            int maxarea = 0;
+            while (left < right)
+            {
+                int min = Math.Min(height[left], height[right]);
+                maxarea = Math.Max(min * (right - left), maxarea);
+                if (height[left] <= height[right])
+                {
+                    ++left;
+                }
+                else
+                {
+                    --right;
+                }
+            }
+
+            return maxarea;
+        }
+    }
+}

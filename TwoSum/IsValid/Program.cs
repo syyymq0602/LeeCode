4dec2dc308af658e34af4606939f3815450c2f35
tpl <tpl_vis_group@outlook.com>
2021-04-11 11:32:15 +0800

    LeetCode 20  字典 栈

---
+++ TwoSum/IsValid/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IsValid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "()[]{}(]";
+            var res = IsValid(s);
+            Console.WriteLine(res.ToString());
+        }
+        // 字典 栈 的使用！！！g'i
+        public static bool IsValid(string s)
+        {
+            int len = s.Length;
+            if (len % 2 == 1)
+                return false;
+            Dictionary<char,char> map = new Dictionary<char, char>
+            {
+                { '}', '{' },
+                { ']', '[' },
+                { ')', '(' }
+            };
+            var chars = s.ToCharArray();
+            var stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                var c = chars[i];
+                if (map.ContainsKey(c))
+                {
+                    var top = stack.Any() ? stack.Pop() : '#';
+                    if (top != map[c])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+            return !stack.Any();
+        }
+    }
+}

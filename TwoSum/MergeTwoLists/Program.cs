626fd3955fb23697986756eb828fb5382106bd94
tpl <tpl_vis_group@outlook.com>
2021-04-19 20:57:04 +0800

    LeetCode 21 链表

---
+++ TwoSum/MergeTwoLists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MergeTwoLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            ListNode prehead = new ListNode(-1);
+            ListNode prev = prehead;
+            while (l1!=null && l2!=null)
+            {
+                if (l1.val <= l2.val)
+                {
+                    prev.next = l1;
+                    l1 = l1.next;
+                }else
+                {
+                    prev.next = l2;
+                    l2 = l2.next;
+                }
+                prev = prev.next;
+            }
+            prev.next = l1 == null ? l2 : l1;
+            return prehead.next;
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+    
+}
